1d1b32b14d0e198266897def5d9aa5b99e629fb9
elinoza <hillcakmak@gmail.com>
2024-06-12 19:39:00 +0300

    layout built

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,109 @@
-import Image from "next/image";
-
+"use client";
+import React from "react";
+import { works } from "../utils/works";
+import { FaLinkedin, FaGithub, FaAngleDoubleDown } from "react-icons/fa";
+import { FaMedium } from "react-icons/fa6";
+import { SiGmail } from "react-icons/si";
 export default function Home() {
+  console.log(works);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <main className="container flex-col mx-auto  ">
+      <header className="text-stone-400 fixed top-0 left-0 right-0 py-3 px-24 my-5 flex items-center text-xl justify-end">
+        {/* {"🌙"}{" "} */}
+        <button className="rounded-3xl bg-stone-200 p-2 px-3 text-stone-800">
+          Contact me
+        </button>
+      </header>
+      <div className=" min-h-[350px] mt-64 ">
+        <h1 className="text-3xl leading-relaxed"> </h1>
+        <p className="text-3xl leading-relaxed mb-16">
+          {" "}
+          Hi there! I'm Hilal, a frontend developer who loves learning and
+          improving. I enjoy contributing open-source projects and writing to
+          share what I know. While I'm still gaining experience, I'm eager to
+          contribute and grow creatively!{" "}
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+        <div className="flex gap-x-3 justify-start text-2xl p-3">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://github.com/elinoza"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            <FaGithub />{" "}
+          </a>
+          <a href="mailto:hillcakmak@gmail.com" rel="noopener noreferrer">
+            <SiGmail />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/hilalsemercioglu/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://medium.com/@hilalsem"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaMedium />{" "}
           </a>
         </div>
       </div>
+      <div className="my-36 ">
+        <hr />
+        <h1 className="text-5xl  py-5 mt-5">Work</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+          {" "}
+          {works &&
+            works.map((work) => (
+              <div className="bg-stone-100 min-h-64 rounded p-3 my-3 flex flex-col  justify-between ">
+                <div>
+                  {" "}
+                  <h1 className="text-xl mb-2">{work.projectName}</h1>
+                  <p>{work.Description} </p>
+                </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+                <div>
+                  {work.Tools.map((tool) => (
+                    <span>{tool} </span>
+                  ))}
+                </div>
+              </div>
+            ))}
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
+      <hr />
+      <footer className="text-xl mt-16 flex justify-between">
+        {" "}
+        <a href="mailto:hillcakmak@gmail.com" rel="noopener noreferrer">
+          <span>Say hi! {"☕️"}</span>
         </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className="flex gap-x-3 items-center ">
+          {" "}
+          <a
+            href="https://github.com/elinoza"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />{" "}
+          </a>
+          <a
+            href="https://www.linkedin.com/in/hilalsemercioglu/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://medium.com/@hilalsem"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaMedium />{" "}
+          </a>
+        </div>
+      </footer>
     </main>
   );
 }
---
+++ utils/works.ts
@@ -0,0 +1,53 @@
+export const works = [
+  {
+    projectName: "Markdown Editor",
+    PosterUrl:
+      "https://private-user-images.githubusercontent.com/72200043/336624351-8c4a0124-8af0-4805-941a-567b89600b21.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTgyMDkzMzgsIm5iZiI6MTcxODIwOTAzOCwicGF0aCI6Ii83MjIwMDA0My8zMzY2MjQzNTEtOGM0YTAxMjQtOGFmMC00ODA1LTk0MWEtNTY3Yjg5NjAwYjIxLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA2MTIlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNjEyVDE2MTcxOFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTUyZGNlZmQ3M2MyYmMxOGQxZTM1M2U4YmU0MGI3ZGYyOThiMjQ0MzYyYmU3YmNhZDcxODA1ZjcwZWU2MWQwMWUmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.QcJvTNozgk4lyyAfcvcQDScQ8W0KiewQnQs8Gfu5jt8",
+    LogoUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/5/52/LinkedIn_Learning_logo.png",
+    Description: "A markdown-editor with synchronized preview built with ",
+    Tools: ["NextJS,Typescript"],
+    WebUrl: "https://markdown-editor-eta-one.vercel.app/",
+    Fe: "https://github.com/elinoza/markdown-editor",
+    Be: "",
+  },
+  {
+    projectName: "Linkedin Learning Clone",
+    PosterUrl:
+      "https://res.cloudinary.com/elinoza/image/upload/v1700477559/DALL_E_2023-11-20_11.52.44_-_A_softly-colored_image_of_an_open_laptop_on_a_pastel-colored_desk._The_laptop_displays_the_LinkedIn_Learning_website_with_its_screen_showing_a_gentle_wybqz0.png",
+    LogoUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/5/52/LinkedIn_Learning_logo.png",
+    Description:
+      "LinkedIn Learning website clone for watching training videos, skill acquisition, and progress tracking. ",
+    Tools: ["ReactJS", "MongoDB"],
+    WebUrl: "https://www.e-learning-platform-fe.vercel.app/",
+    Fe: "https://github.com/elinoza/e-learning-platform-fe",
+    Be: "https://github.com/elinoza/e-learning-platform-be",
+  },
+  {
+    projectName: "Weather App",
+    PosterUrl:
+      "https://res.cloudinary.com/elinoza/image/upload/v1700552389/DALL_E_2023-11-21_10.31.44_-_In_a_minimal_and_soft_environment_a_woman_is_seen_from_a_slight_rear_angle_looking_at_her_laptop_with_an_umbrella_beside_her._The_laptop_displays_a_up8mzu.png",
+    LogoUrl:
+      "https://i.pinimg.com/564x/75/b3/3e/75b33e0bfbc8b13cd646dca30511f30a.jpg",
+    Description:
+      "Weather App is a web application that provides weather information for cities around the world.",
+    Tools: ["ReactJS", "MongoDB"],
+    WebUrl: "https://weather-app-elinoza.vercel.app",
+    Fe: "https://github.com/elinoza/Weather-App",
+    Be: "https://github.com/elinoza/weather-app-be",
+  },
+  {
+    projectName: "Quiz Game",
+    PosterUrl:
+      "https://res.cloudinary.com/elinoza/image/upload/v1700552276/DALL_E_2023-11-21_10.35.56_-_A_young_child_in_their_bedroom_slightly_excited_playing_a_quiz_on_their_laptop._The_child_is_seen_from_a_side_angle_and_the_laptop_screen_has_the_w_yutgnt.png",
+    LogoUrl:
+      "https://img.freepik.com/free-vector/comic-style-question-mark-speech-bubble-background_1017-23978.jpg?w=826&t=st=1700332942~exp=1700333542~hmac=af665bb2cd39b784aaaf37bba6a7189c9b73fc13d5f425f5c069cca18e426423",
+    Description:
+      "This is a JavaScript quiz application that asks users questions and provides them with scores.",
+    Tools: ["ReactJS", "MongoDB"],
+    WebUrl: "https://quiz-fe-elinoza.vercel.app/",
+    Fe: "https://github.com/elinoza/quiz-fe",
+    Be: "https://github.com/elinoza/quiz-be",
+  },
+];
