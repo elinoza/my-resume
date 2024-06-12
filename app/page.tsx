1ed090de56afc4f5f8a5751a06e6190204335004
elinoza <hillcakmak@gmail.com>
2024-06-13 01:53:42 +0300

    almost done

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,27 @@
 "use client";
 import React from "react";
-import { works } from "../utils/works";
+import clsx from "clsx";
+
+import { works, logos } from "../utils/works";
 import { FaLinkedin, FaGithub, FaAngleDoubleDown } from "react-icons/fa";
 import { FaMedium } from "react-icons/fa6";
 import { SiGmail } from "react-icons/si";
+
 export default function Home() {
   console.log(works);
   return (
     <main className="container flex-col mx-auto  ">
       <header className="text-stone-400 fixed top-0 left-0 right-0 py-3 px-24 my-5 flex items-center text-xl justify-end">
-        {/* {"🌙"}{" "} */}
-        <button className="rounded-3xl bg-stone-200 p-2 px-3 text-stone-800">
+        <button className="rounded-3xl bg-stone-800 p-2 px-3 text-stone-200">
           Contact me
         </button>
       </header>
-      <div className=" min-h-[350px] mt-64 ">
+      <div className=" min-h-[350px] mt-72 ">
         <h1 className="text-3xl leading-relaxed"> </h1>
         <p className="text-3xl leading-relaxed mb-16">
           {" "}
-          Hi there! I'm Hilal, a frontend developer who loves learning and
-          improving. I enjoy contributing open-source projects and writing to
-          share what I know. While I'm still gaining experience, I'm eager to
-          contribute and grow creatively!{" "}
+          "Greetings! I'm Hilal, a frontend wizard who has a spell for turning
+          coffee into code with my favorite spell: 'Caffeinum Codex!"
         </p>
         <div className="flex gap-x-3 justify-start text-2xl p-3">
           <a
@@ -51,13 +51,22 @@ export default function Home() {
         </div>
       </div>
       <div className="my-36 ">
-        <hr />
-        <h1 className="text-5xl  py-5 mt-5">Work</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+        <hr className="border-zinc-800" />
+        <h1 className="text-7xl  py-5 my-7">Work</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2  gap-10">
           {" "}
           {works &&
-            works.map((work) => (
-              <div className="bg-stone-100 min-h-64 rounded p-3 my-3 flex flex-col  justify-between ">
+            works.map((work, index) => (
+              <a
+                key={index}
+                className={clsx(
+                  // `h-${work.Height}`,
+                  "bg-neutral-100  min-h-56 rounded-xl p-3 my-3 flex flex-col  justify-between"
+                )}
+                href={work.WebUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <div>
                   {" "}
                   <h1 className="text-xl mb-2">{work.projectName}</h1>
@@ -65,15 +74,50 @@ export default function Home() {
                 </div>
 
                 <div>
-                  {work.Tools.map((tool) => (
-                    <span>{tool} </span>
+                  {work.Tools.map((tool, index) => (
+                    <span
+                      className="flex items-center justify-end text-sm"
+                      key={index}
+                    >
+                      {tool}
+                      <div
+                        className={clsx(
+                          {
+                            "bg-blue-500": tool === "Typescript",
+                            "bg-orange-500": tool === "NextJS",
+                            "bg-yellow-500": tool === "ReactJS",
+                            "bg-green-500": tool === "MongoDB",
+                            "bg-rose-500": tool === "ExpressJS",
+                          },
+                          " w-3 h-3 rounded-full inline-block ml-2"
+                        )}
+                      ></div>
+                    </span>
                   ))}
                 </div>
-              </div>
+              </a>
             ))}
         </div>
       </div>
-      <hr />
+      <hr className="border-zinc-800 my-36" />
+
+      <>
+        {" "}
+        <div className="flex justify-end  ">
+          <h1 className="text-7xl py-5 ">Skill Set</h1>
+        </div>
+        <div className="flex gap-3 justify-end mb-64 ">
+          {logos.map((logo) => (
+            <div>
+              {logo[0]}{" "}
+              <span>
+                <img src={logo[1]} />
+              </span>
+            </div>
+          ))}
+        </div>
+      </>
+
       <footer className="text-xl mt-16 flex justify-between">
         {" "}
         <a href="mailto:hillcakmak@gmail.com" rel="noopener noreferrer">
--- utils/works.ts
+++ utils/works.ts
@@ -3,51 +3,94 @@ export const works = [
     projectName: "Markdown Editor",
     PosterUrl:
       "https://private-user-images.githubusercontent.com/72200043/336624351-8c4a0124-8af0-4805-941a-567b89600b21.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTgyMDkzMzgsIm5iZiI6MTcxODIwOTAzOCwicGF0aCI6Ii83MjIwMDA0My8zMzY2MjQzNTEtOGM0YTAxMjQtOGFmMC00ODA1LTk0MWEtNTY3Yjg5NjAwYjIxLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA2MTIlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNjEyVDE2MTcxOFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTUyZGNlZmQ3M2MyYmMxOGQxZTM1M2U4YmU0MGI3ZGYyOThiMjQ0MzYyYmU3YmNhZDcxODA1ZjcwZWU2MWQwMWUmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.QcJvTNozgk4lyyAfcvcQDScQ8W0KiewQnQs8Gfu5jt8",
-    LogoUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/5/52/LinkedIn_Learning_logo.png",
-    Description: "A markdown-editor with synchronized preview built with ",
-    Tools: ["NextJS,Typescript"],
+    Description: "A markdown-editor with synchronized preview. ",
+    Tools: ["NextJS", "Typescript"],
     WebUrl: "https://markdown-editor-eta-one.vercel.app/",
     Fe: "https://github.com/elinoza/markdown-editor",
+    Height: "64",
+    Be: "",
+  },
+  {
+    projectName: "Calculator",
+    PosterUrl:
+      "https://private-user-images.githubusercontent.com/72200043/336624351-8c4a0124-8af0-4805-941a-567b89600b21.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTgyMDkzMzgsIm5iZiI6MTcxODIwOTAzOCwicGF0aCI6Ii83MjIwMDA0My8zMzY2MjQzNTEtOGM0YTAxMjQtOGFmMC00ODA1LTk0MWEtNTY3Yjg5NjAwYjIxLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA2MTIlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNjEyVDE2MTcxOFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTUyZGNlZmQ3M2MyYmMxOGQxZTM1M2U4YmU0MGI3ZGYyOThiMjQ0MzYyYmU3YmNhZDcxODA1ZjcwZWU2MWQwMWUmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.QcJvTNozgk4lyyAfcvcQDScQ8W0KiewQnQs8Gfu5jt8",
+    Description:
+      "A calculator app which is mimicking the functionality of the macOS Calculator. ",
+    Tools: ["NextJS", "Typescript"],
+    WebUrl: "https://calculator-lac-alpha.vercel.app/",
+    Fe: "https://github.com/elinoza/calculator",
+    Height: "52",
+    Be: "",
+  },
+  {
+    projectName: "TaDaa App ",
+    PosterUrl:
+      "https://private-user-images.githubusercontent.com/72200043/336624351-8c4a0124-8af0-4805-941a-567b89600b21.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTgyMDkzMzgsIm5iZiI6MTcxODIwOTAzOCwicGF0aCI6Ii83MjIwMDA0My8zMzY2MjQzNTEtOGM0YTAxMjQtOGFmMC00ODA1LTk0MWEtNTY3Yjg5NjAwYjIxLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA2MTIlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNjEyVDE2MTcxOFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTUyZGNlZmQ3M2MyYmMxOGQxZTM1M2U4YmU0MGI3ZGYyOThiMjQ0MzYyYmU3YmNhZDcxODA1ZjcwZWU2MWQwMWUmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.QcJvTNozgk4lyyAfcvcQDScQ8W0KiewQnQs8Gfu5jt8",
+    Description:
+      "TaDaa app is a simple task manager that allows you to write down tasks, mark them as completed, delete them, and edit them.",
+    Tools: ["NextJS", "Typescript"],
+    WebUrl: "https://todoapp-eight-weld.vercel.app/",
+    Fe: "https://github.com/elinoza/tadaa-App",
+    Height: "48",
+    Be: "",
+  },
+  {
+    projectName: "Pomodoro Tracker",
+    PosterUrl:
+      "https://private-user-images.githubusercontent.com/72200043/336624351-8c4a0124-8af0-4805-941a-567b89600b21.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTgyMDkzMzgsIm5iZiI6MTcxODIwOTAzOCwicGF0aCI6Ii83MjIwMDA0My8zMzY2MjQzNTEtOGM0YTAxMjQtOGFmMC00ODA1LTk0MWEtNTY3Yjg5NjAwYjIxLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA2MTIlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNjEyVDE2MTcxOFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTUyZGNlZmQ3M2MyYmMxOGQxZTM1M2U4YmU0MGI3ZGYyOThiMjQ0MzYyYmU3YmNhZDcxODA1ZjcwZWU2MWQwMWUmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.QcJvTNozgk4lyyAfcvcQDScQ8W0KiewQnQs8Gfu5jt8",
+    Description:
+      "A simple Pomodoro Tracker which you can enhance your productivity by breaking your work into intervals,  25 minutes in length, separated by 5 in short breaks. ",
+    Tools: ["NextJS", "Typescript"],
+    WebUrl: "https://pomodoro-alpha-ecru.vercel.app/",
+    Fe: "https://github.com/elinoza/pomodoro",
+    Height: "64",
     Be: "",
   },
   {
     projectName: "Linkedin Learning Clone",
     PosterUrl:
       "https://res.cloudinary.com/elinoza/image/upload/v1700477559/DALL_E_2023-11-20_11.52.44_-_A_softly-colored_image_of_an_open_laptop_on_a_pastel-colored_desk._The_laptop_displays_the_LinkedIn_Learning_website_with_its_screen_showing_a_gentle_wybqz0.png",
-    LogoUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/5/52/LinkedIn_Learning_logo.png",
     Description:
       "LinkedIn Learning website clone for watching training videos, skill acquisition, and progress tracking. ",
-    Tools: ["ReactJS", "MongoDB"],
+    Tools: ["ReactJS", "MongoDB", "ExpressJS"],
     WebUrl: "https://www.e-learning-platform-fe.vercel.app/",
     Fe: "https://github.com/elinoza/e-learning-platform-fe",
+    Height: "52",
     Be: "https://github.com/elinoza/e-learning-platform-be",
   },
   {
     projectName: "Weather App",
     PosterUrl:
       "https://res.cloudinary.com/elinoza/image/upload/v1700552389/DALL_E_2023-11-21_10.31.44_-_In_a_minimal_and_soft_environment_a_woman_is_seen_from_a_slight_rear_angle_looking_at_her_laptop_with_an_umbrella_beside_her._The_laptop_displays_a_up8mzu.png",
-    LogoUrl:
-      "https://i.pinimg.com/564x/75/b3/3e/75b33e0bfbc8b13cd646dca30511f30a.jpg",
     Description:
       "Weather App is a web application that provides weather information for cities around the world.",
-    Tools: ["ReactJS", "MongoDB"],
+    Tools: ["ReactJS", "MongoDB", "ExpressJS"],
     WebUrl: "https://weather-app-elinoza.vercel.app",
     Fe: "https://github.com/elinoza/Weather-App",
+    Height: "64",
     Be: "https://github.com/elinoza/weather-app-be",
   },
-  {
-    projectName: "Quiz Game",
-    PosterUrl:
-      "https://res.cloudinary.com/elinoza/image/upload/v1700552276/DALL_E_2023-11-21_10.35.56_-_A_young_child_in_their_bedroom_slightly_excited_playing_a_quiz_on_their_laptop._The_child_is_seen_from_a_side_angle_and_the_laptop_screen_has_the_w_yutgnt.png",
-    LogoUrl:
-      "https://img.freepik.com/free-vector/comic-style-question-mark-speech-bubble-background_1017-23978.jpg?w=826&t=st=1700332942~exp=1700333542~hmac=af665bb2cd39b784aaaf37bba6a7189c9b73fc13d5f425f5c069cca18e426423",
-    Description:
-      "This is a JavaScript quiz application that asks users questions and provides them with scores.",
-    Tools: ["ReactJS", "MongoDB"],
-    WebUrl: "https://quiz-fe-elinoza.vercel.app/",
-    Fe: "https://github.com/elinoza/quiz-fe",
-    Be: "https://github.com/elinoza/quiz-be",
-  },
+];
+
+export const logos = [
+  [
+    "ReactJS",
+    "https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB",
+  ],
+  [
+    "NodeJS",
+    "https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white",
+  ],
+  [
+    "GitHub",
+    "https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white",
+  ],
+  [
+    "ExpressJS",
+    "https://img.shields.io/badge/Express.js-404D59?style=for-the-badge",
+  ],
+  [
+    "MongoDB",
+    "https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white",
+  ],
 ];
